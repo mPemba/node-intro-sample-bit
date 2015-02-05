9f66266b42283e55264bec7de75e604e5ee423d4
Michael Sayer <michael.j.say@gmail.com>
2015-02-05 12:15:55 -0700

    sample

---
+++ smurfs.js
@@ -0,0 +1,18 @@
+
+// exports.varName = {} | 3 | function
+
+// module.exports = {
+// 	doSomething: function() {
+// 		console.log('party on wayne');
+// 	}
+// }
+
+// exports.doSomething = function() {console.log('yes');}
+
+exports.me = {
+	name: 'mike',
+	age: 24, 
+	job: 'none', 
+	says: 'party on garth!',
+	res: 'party on wayne'
+}
